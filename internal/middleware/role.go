1062e31058cad67f94d11100b25b80a1299101ef
Ilya <Em0zzIoN@yandex.ru>
2025-03-26 18:52:52 +0300

    feat: add role middleware

---
+++ internal/middleware/role.go
@@ -0,0 +1,26 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Проверка соответствия роли
+func RoleMiddleware(requiredRole string) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		roleVal, exists := c.Get("role")
+		if !exists {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Role not found"})
+			return
+		}
+
+		role, ok := roleVal.(string)
+		if !ok || role != requiredRole {
+			c.AbortWithStatusJSON(http.StatusForbidden, gin.H{"error": "Access denied"})
+			return
+		}
+
+		c.Next()
+	}
+}
